e815c8fc7e653abc4cb45419a1add81586fa07ff
rreddyga <ramanag2299@gmail.com>
2026-03-21 22:30:22 +0530

    changes are made

--- 30-bastion/main.tf
+++ 30-bastion/main.tf
@@ -7,9 +7,22 @@ resource "aws_instance" "bastion" {
 
   #sg group id
   vpc_security_group_ids = [local.bastion_sg_id]
-
+  
   iam_instance_profile = aws_iam_instance_profile.bastion.name
-
+  user_data = file(bastion.sh)
+  #we need to increase the memory of bastion host to install the terraform as there are more instances we will create
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
+     # Ebs volum tags
+    tags =  merge(
+    {
+        Name = "${var.project}-${var.environment}-bastion"
+    },
+    local.common_tags
+  )
+  }
+ 
   tags = merge(
     {
         Name = "${var.project}-${var.environment}-bastion"
