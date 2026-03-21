81455f08530167b22e69f0023bae0f5a1806ff86
rreddyga <ramanag2299@gmail.com>
2026-03-21 23:00:44 +0530

    bastion script added to extend the memory and install the terraform

--- 30-bastion/bastion.sh
+++ 30-bastion/bastion.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
-growpart /dev/nvme0n1p4 4
-lvextend -r -L +30G /dev/mapper/RootVG-homeVol
-xfs_growfs /home
+yum update -y
+
+# Detect NVMe device (root disk)
+ROOT_DISK=$(lsblk -dno NAME,SIZE,MOUNTPOINT | grep '[0-9]\+G /$' | awk '{print "/dev/" $1}')
+if [ -z "$ROOT_DISK" ]; then
+  echo "Root disk not found!"
+  exit 1
+fi
+echo "Resizing $ROOT_DISK..."
+
+# Install tools if needed
+yum install -y cloud-utils-growpart lvm2 xfsprogs
+
+# Extend partition (p3 for LVM PV on RHEL; common GPT layout)
+growpart "$ROOT_DISK" 3
+
+# Rescan PV, extend VG/LV, resize FS (root usually /dev/mapper/RootVG-rootVol or home)
+pvresize "$ROOT_DISK"p3
+lvextend -l +100%FREE -r /dev/RootVG/rootVol  # Adjust LV if /home: /dev/RootVG-homeVol
+# For /home specifically: lvextend -l +100%FREE -r /dev/mapper/RootVG-homeVol
+
+# Verify
+df -h / /home
+lsblk
+
+# Install Terraform
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+yum -y install terraform
+terraform --version
