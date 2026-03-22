5aac6d2c079082c7bbf0ad6b95bc5d3e78f21092
rreddyga <ramanag2299@gmail.com>
2026-03-22 16:15:12 +0530

    redis configurations changed

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -21,9 +21,11 @@ data "aws_ami" "joindevops" {
 data "aws_ssm_parameter" "database_subnet_ids" {
     name = "/${var.project}/${var.environment}/database_subnet_ids"
 } 
-
 #mongodb security group id
-
 data "aws_ssm_parameter" "mongodb_sg_id" {
     name = "/${var.project}/${var.environment}/mongodb_sg_id"
+} 
+#mongodb security group id
+data "aws_ssm_parameter" "redis_sg_id" {
+    name = "/${var.project}/${var.environment}/redis_sg_id"
 } 
--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -9,4 +9,5 @@ locals {
     #database subnet is us-east-1a AZ
     database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+    redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
 }
