381b0b7146e1a37749ffb13748d560ebd50bbabc
rreddyga <ramanag2299@gmail.com>
2026-03-22 20:49:08 +0530

    redis file changes

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -7,3 +7,9 @@ variable "environment" {
     type = string
     default = "dev"
 }
+variable "zone_id" {
+    default = "Z0853835SDE0JTKZUUR2"
+}
+variable "domain_name" {
+    default = "sanathananelaform.online"
+}
