44ab249f81f68a20a5b47faee7aca5d064578d0f
rreddyga <ramanag2299@gmail.com>
2026-03-22 21:09:14 +0530

    redis file changes

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -40,3 +40,15 @@ resource "aws_security_group_rule" "mongodb_user" {
   source_security_group_id =local.user_sg_id
   security_group_id = local.mongodb_sg_id
 }
+
+#redis-bastion
+
+resource "aws_security_group_rule" "redis_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  #cidr_blocks       = [aws_vpc.example.cidr_block]
+  source_security_group_id =local.bastion_sg_id
+  security_group_id = local.redis_sg_id
+}
