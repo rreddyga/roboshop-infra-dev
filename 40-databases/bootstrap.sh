68bb634f98cd2092b3ad5e88cfb41e9852d73101
rreddyga <ramanag2299@gmail.com>
2026-03-22 13:31:20 +0530

    updated the script

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 component=$1
-dnf install ansible -y 
 
-#clone the ansible-roboshop-roles-tf repo
+# Use dnf for RHEL9 (not yum)
+sudo dnf install -y ansible git
+
 cd /home/ec2-user
+
+# Clone repo
+rm -rf ansible-roboshop-roles-tf  # Clean previous
 git clone https://github.com/rreddyga/ansible-roboshop-roles-tf.git
-#change the directory to ansible-roboshop-roles-tf
 cd ansible-roboshop-roles-tf
-#run the ansible playbook
-ansible-playbook -e component=$component roboshop.yaml
+
+# Create inventory for localhost as "{{ component }}" group
+sudo mkdir -p /etc/ansible
+cat > inventory.ini << EOF
+[${component}]
+localhost ansible_connection=local ansible_python_interpreter=/usr/bin/python3
+
+[all:vars]
+ansible_connection=local
+EOF
+
+# Run playbook with inventory
+ansible-playbook -i inventory.ini -e component=$component roboshop.yaml -b -K
+
+echo "Bootstrap complete for $component"
