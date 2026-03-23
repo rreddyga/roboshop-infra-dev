e88d5e3ac7facc92277b531ea36248091521c04a
rreddyga <ramanag2299@gmail.com>
2026-03-23 14:22:02 +0530

    mysql iam roles are added

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -11,5 +11,5 @@ locals {
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
     mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
-    mysql_role_name = join("-" , [ for name in ["${var.project}", "${var.environment}", "Mysql"]:upper(name)])
+    mysql_role_name = join("-" , [ for name in ["${var.project}", "${var.environment}", "Mysql"] : title(name)])
 }
